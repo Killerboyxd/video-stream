d200038540650321d9e2065961ccfdc5a1450c14
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-03-26 22:32:23 +0530

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -16,8 +16,8 @@ def stream_markup(user_id):
       InlineKeyboardButton(text="‣‣I", callback_data=f'set_skip | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/Team_Bot_Support"),
-      InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇs", url=f"https://t.me/team_bot_update"),
+      InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/RONNY_KI_DUNIYA"),
+      InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇs", url=f"https://t.me/do_dil_ek_jaan143"),
     ],
     [
       InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data="set_close"),
