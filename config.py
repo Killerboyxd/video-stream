2f01a537f3a5edca3573cb5ed5a4314511b0a521
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-10-15 23:31:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ BOT_USERNAME = getenv("BOT_USERNAME")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Team-Bot2/video-stream")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "dil_ki_baate_op")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "do_dil_ek_jaan143")
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL")
